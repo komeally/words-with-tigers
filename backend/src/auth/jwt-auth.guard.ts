90d3f917143ffe042e4f00f6427b2fca504e0320
komeally <kasim.omeally@gmail.com>
2024-10-25 16:49:33 -0400

    Setting up a basic version of helmet

--- backend/src/auth/jwt-auth.guard.ts
+++ backend/src/auth/jwt-auth.guard.ts
@@ -1,7 +1,6 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
 import { IS_PUBLIC_KEY } from './public.decorator';
 
 @Injectable()
